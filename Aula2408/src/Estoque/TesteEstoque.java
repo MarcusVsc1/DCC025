692337db356ea504d8bf77b64781230a27087ae4
ice <ice@lab06m49.nrc.ice.ufjf.br>
2018-08-24 20:48:55 -0300

    Primeira versão: Implementa a main com quatro produtos diferentes.

---
+++ Aula2408/src/Estoque/TesteEstoque.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Estoque;
+
+/**
+ *
+ * @author ice
+ */
+public class TesteEstoque {
+    public static void main(String[] args){
+        Estoque produto1 = new Estoque("Impressora a jato",13,6);
+        Estoque produto2 = new Estoque("Monitor LCD 17 polegadas",11,13);
+        Estoque produto3 = new Estoque("Mouse optico",6,2);
+        Estoque produto4 = new Estoque("HD Externo",20,8);
+    }
+}

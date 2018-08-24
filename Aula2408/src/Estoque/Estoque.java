d92b49c65b98f54bfa59588082e7d97dc719c2b7
ice <ice@lab06m49.nrc.ice.ufjf.br>
2018-08-24 20:48:36 -0300

    Primeira versão: Implementa as variáveis do estoque, construtor, setters e outras funções uteis.

--- Aula2408/src/Estoque/Estoque.java
+++ Aula2408/src/Estoque/Estoque.java
@@ -5,10 +5,63 @@
  */
 package Estoque;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ice
  */
 public class Estoque {
+
+    private String nome;
+    private int quantidadeAtual;
+    private int quantidadeMinima;
+
+    public Estoque(String nome, int quantidadeAtual, int quantidadeMinima) {
+        this.nome = nome;
+        this.quantidadeAtual = quantidadeAtual;
+        this.quantidadeMinima = quantidadeMinima;
+    }
+
+    public void alteraNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void mudaQuantidadeAtual(int quantidadeAtual) {
+        this.quantidadeAtual = quantidadeAtual;
+    }
+
+    public void mudaQuantidadeMinima(int quantidadeMinima) {
+        this.quantidadeMinima = quantidadeMinima;
+    }
     
+    public void imprimeProduto(){
+        System.out.println("Dados do produto. ");
+        System.out.println("Nome: "+this.nome);
+        System.out.println("Quantidade atual: "+this.quantidadeAtual);
+        System.out.println("Quantidade Minima: "+this.quantidadeMinima); 
+    }
+    
+    public void darBaixa(){
+        Scanner num = new Scanner(System.in);
+        int n = num.nextInt();
+        if(n>quantidadeAtual){
+            System.out.println("Quantidade digitada é maior do que o estoque!");
+        }else{            
+         quantidadeAtual-=n;
+         System.out.println("Foi dada baixa de "+n+" produtos do estoque de "+this.nome);
+        }
+    }
+    public void reporEstoque(int n){
+        Scanner num = new Scanner(System.in);
+        int n2 = num.nextInt();
+        quantidadeAtual+=n2;
+        System.out.println("Foi aumentado em "+n+" a quantidade de produtos do estoque de "+this.nome);
+    }
+    
+    public void verificarReposicao(){
+        if(quantidadeAtual<quantidadeMinima){
+            System.out.println(this.nome+" precisa ser resposto em "+(quantidadeMinima-quantidadeAtual)+" itens");
+        }
+    }
 }

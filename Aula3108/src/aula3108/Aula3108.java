737b3a1049b6368aa5ae055070bbdd56f2b1d424
MarcusVsc1 <37600192+MarcusVsc1@users.noreply.github.com>
2018-08-31 20:49:50 -0300

    Add files via upload
    Primeira versão: Jogo Bozó. Cria a main, classes Jogo, Dado e Jogador.

---
+++ Aula3108/src/aula3108/Aula3108.java
@@ -0,0 +1,14 @@
+package aula3108;
+
+/**
+ *
+ * @author Marcus
+ */
+public class Aula3108 {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Aula3108/src/aula3108/Dado.java
@@ -0,0 +1,23 @@
+    /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula3108;
+
+/**
+ *
+ * @author Marcus
+ */
+public class Dado {
+    private int roll;
+    
+    public void retornaDado(int n){
+        switch(n){
+            case 1:
+                break;
+                
+                
+        }
+    }
+}
